11df4b4b76f3255e9ff6b2fa82121a7b84af11e2
APOORVA DUBEY <winwithapoorva@gmail.com>
2015-10-15 00:59:05 -0700

    Chaged the DB URL to use EC2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
 
 	// the database url to connect
-	url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
+	// url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
+	url : 'mongodb://eddie:eddie@ec2-52-89-45-190.us-west-2.compute.amazonaws.com:27017/dummyDB'
 }
